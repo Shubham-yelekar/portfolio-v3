53e0c539f8459320c9fe61fc22f0aaebe6b880b6
Shubham Yelekar <shubhamyelekar0@gmail.com>
2025-09-28 19:52:47 +0530

    fixed typescript error

--- src/components/ui/WavingFlag.tsx
+++ src/components/ui/WavingFlag.tsx
@@ -1,25 +1,39 @@
 "use client";
 import React from "react";
-import { useRef, useMemo } from "react";
+import { useRef, useMemo, type FC } from "react";
 
 ///  THREE D
 import * as THREE from "three";
-import { Canvas, useFrame, extend } from "@react-three/fiber";
-import { OrbitControls, shaderMaterial, useTexture } from "@react-three/drei";
+import {
+  Canvas,
+  useFrame,
+  extend,
+  type ShaderMaterialProps,
+} from "@react-three/fiber";
+import { shaderMaterial, useTexture } from "@react-three/drei";
 
 /// Shaders
 import vertexShader from "../../../public/Shaders/WavingFlag/flagVertex.glsl";
 import fragmentShader from "../../../public/Shaders/WavingFlag/flagFragment.glsl";
 
+type Uniforms = {
+  uTime: number;
+  uElevation: number;
+  uFrequency: THREE.Vector2;
+  uTexture: THREE.Texture;
+  uLightDirection: THREE.Vector3;
+};
+
+const DEFAULT_UNIFORMS: Uniforms = {
+  uTime: 0,
+  uElevation: 0.3,
+  uFrequency: new THREE.Vector2(16 / 8),
+  uTexture: new THREE.Texture(),
+  uLightDirection: new THREE.Vector3(-2.8, 0.17, 8.0),
+};
+
 const WavingFlagMaterial = shaderMaterial(
-  {
-    //Uniforms
-    uTime: 0,
-    uElevation: 0.3,
-    uFrequency: new THREE.Vector2(16 / 8),
-    uTexture: new THREE.Texture(),
-    uLightDirection: new THREE.Vector3(-2.8, 0.17, 8.0),
-  },
+  DEFAULT_UNIFORMS,
   vertexShader,
   fragmentShader,
 );
@@ -28,11 +42,9 @@ extend({ WavingFlagMaterial });
 
 // Define the specific props (uniforms) for your material
 
-declare global {
-  namespace TSX {
-    interface IntrinsixElements {
-      wavingFlagMaterial: any;
-    }
+declare module "@react-three/fiber" {
+  interface ThreeElements {
+    wavingFlagMaterial: ShaderMaterialProps & Uniforms;
   }
 }
 
@@ -82,7 +94,8 @@ function Scene() {
   );
 }
 
-const WavingFlag = () => {
+const WavingFlag: FC = () => {
+  const shaderRef = useRef<THREE.ShaderMaterial & Uniforms>(null!);
   return (
     <div className="fade-mask absolute -top-14 left-1/2 -z-1 h-96 w-[900px] -translate-x-1/2 bg-blue-400">
       <Canvas
@@ -96,7 +109,7 @@ const WavingFlag = () => {
         }}
         style={{ background: "#e5e5e5", height: "60vh" }}
       >
-        <OrbitControls enableDamping />
+        {/* <OrbitControls enableDamping /> */}
         <Scene />
       </Canvas>
     </div>
