669ef9fce33295207f17d50caa117cf798fcf541
Shubham Yelekar <shubhamyelekar0@gmail.com>
2025-12-31 13:53:00 +0530

    accordion thumbnail changes

--- src/app/(main)/notes/page.tsx
+++ src/app/(main)/notes/page.tsx
@@ -7,7 +7,7 @@ import NotesFilter from "@/components/sections/NotesFilter";
 export default async function NotesPage() {
   const posts = getAllContentMeta("notes");
   const livePosts = posts
-    .filter((post) => post.status === "draft")
+    .filter((post) => post.status === "live")
     .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
   const uniqueTags = [...new Set(livePosts.flatMap((posts) => posts.tags))];
 
