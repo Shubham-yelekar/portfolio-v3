c63a8448a16b513277c0a2798f5f5bede5f4f8a9
Shubham Yelekar <shubhamyelekar0@gmail.com>
2026-02-18 13:43:25 +0530

    Revert "animations"
    This reverts commit 2eb34f0054f3b01cd3da8b6c6008e45e7e55e141.

--- src/components/ui-components/tabs/tabsContent/TabsContent.tsx
+++ src/components/ui-components/tabs/tabsContent/TabsContent.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { useTabsContext } from "../TabContext";
-import { motion, AnimatePresence } from "motion/react";
+
 type TabsContentProps = {
   value: string;
   children: React.ReactNode;
@@ -12,22 +12,13 @@ export function TabsContent({ value, children, className }: TabsContentProps) {
   const { value: activeValue } = useTabsContext();
 
   if (value !== activeValue) return null;
-  const isActive = value === activeValue;
 
   return (
-    <AnimatePresence>
-      {isActive && (
-        <motion.div
-          role="tabpanel"
-          initial={{ opacity: 0, y: 8 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -8 }}
-          transition={{ duration: 0.4, ease: "easeOut" }}
-          className={`text-paragraph mt-1 rounded-xl border border-neutral-300 bg-neutral-200 p-3 dark:border-neutral-700/20 dark:bg-neutral-800${className ?? ""}`}
-        >
-          {children}
-        </motion.div>
-      )}
-    </AnimatePresence>
+    <div
+      role="tabpanel"
+      className={`text-paragraph mt-1 rounded-xl border border-neutral-300 bg-neutral-200 p-3 dark:border-neutral-700/20 dark:bg-neutral-800${className ?? ""}`}
+    >
+      {children}
+    </div>
   );
 }
