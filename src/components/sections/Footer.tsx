0a759f3f050a116b0923964e2e98f3e53daa11c3
Shubham Yelekar <shubhamyelekar0@gmail.com>
2026-02-19 10:27:16 +0530

    Update Footer.tsx

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -7,19 +7,20 @@ import { FaReact } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <footer className="mt-12 flex min-h-[25dvh] items-center justify-center overflow-hidden">
+    <footer className="mt-12 flex min-h-[25dvh] flex-col items-center justify-center gap-4 overflow-hidden border-t border-neutral-400">
+      <p className="text-sm">Connect with me on</p>
       <div className="z-1 flex gap-4">
         <Link href={"https://x.com/shubu_y"} target="_blank">
-          <BsTwitterX size={16} />
+          <BsTwitterX size={18} />
         </Link>
         <Link href={"https://github.com/Shubham-yelekar"} target="_blank">
-          <BsGithub size={16} />
+          <BsGithub size={18} />
         </Link>
         <Link
           href={"https://www.linkedin.com/in/shubhamyelekar/"}
           target="_blank"
         >
-          <BsLinkedin size={16} />
+          <BsLinkedin size={18} />
         </Link>
       </div>
     </footer>
