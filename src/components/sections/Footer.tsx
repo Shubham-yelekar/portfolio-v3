e94ad07cca7e301aa0f673436c18541364bdfafe
Shubham Yelekar <shubhamyelekar0@gmail.com>
2026-04-15 16:48:07 +0530

    Revert "Update Footer.tsx"
    This reverts commit 0a759f3f050a116b0923964e2e98f3e53daa11c3.

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -7,20 +7,19 @@ import { FaReact } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <footer className="mt-12 flex min-h-[25dvh] flex-col items-center justify-center gap-4 overflow-hidden border-t border-neutral-400">
-      <p className="text-sm">Connect with me on</p>
+    <footer className="mt-12 flex min-h-[25dvh] items-center justify-center overflow-hidden">
       <div className="z-1 flex gap-4">
         <Link href={"https://x.com/shubu_y"} target="_blank">
-          <BsTwitterX size={18} />
+          <BsTwitterX size={16} />
         </Link>
         <Link href={"https://github.com/Shubham-yelekar"} target="_blank">
-          <BsGithub size={18} />
+          <BsGithub size={16} />
         </Link>
         <Link
           href={"https://www.linkedin.com/in/shubhamyelekar/"}
           target="_blank"
         >
-          <BsLinkedin size={18} />
+          <BsLinkedin size={16} />
         </Link>
       </div>
     </footer>
